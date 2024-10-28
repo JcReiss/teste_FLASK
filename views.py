61b924581acbe550936f8fea21ee0110d1f3dc99
JcReiss <cesinhajunior17@hotmail.com>
2024-10-28 18:22:14 -0400

    deu certo

--- views.py
+++ views.py
@@ -6,3 +6,9 @@ from main import app, render_template
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+
+#? rota "sobre"
+@app.route("/sobre")
+def rota_sobre():
+    return "conheça nosso site"
