1f5949ab1b5b638a4596ea397841c3ba361a8acf
JcReiss <cesinhajunior17@hotmail.com>
2024-10-30 21:19:23 -0400

    teste

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask,url_for, render_template
-
+from flask import blueprints
 
 app =  Flask(__name__)
 
-from views import *
+from routes.views import *
+
 
 if __name__ == "__main__":
     app.run(debug=True) 
